530fb68b61d755dbc0f23bab0eea19731a740e3f
MaxNicol <nicol.maxime@gmail.com>
2019-10-07 15:13:48 -0400

    Ajout labo3

---
+++ labo3/src/main/java/Taxes.java
@@ -0,0 +1,13 @@
+public class Taxes {
+
+    public static long calculTaxes(long sous,
+                                   boolean taxeFed, boolean taxePro){
+        long resultat = sous;
+        if (taxeFed)resultat += Math.round(sous * .05);
+        if (taxePro)resultat += Math.round(sous * .09975);
+
+        return resultat;
+    }
+
+
+}
